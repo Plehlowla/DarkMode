41f1a24c32bf6b00b6cbd32f819fe3f049be3185
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-05-05 06:32:39 -0500

    render items

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 
 
 const btn = document.querySelector('.btn');
-const sectionArticle = document.querySelectorAll('.articles')
+const articlesDOM = document.querySelector('.articles')
 
 btn.addEventListener('click', () => {
     // console.log('test')
     document.documentElement.classList.toggle('dark-theme');
 });
+
+const articlesData = articles.map((item) => {
+    // console.log(item)
+    const {title, date, length, snippet} = item;
+    // format date
+    return `<article class="post">
+    <h2>${title}</h2>
+    <div class="post-info">
+      <span>${date}</span>
+      <span${length}</span>
+    </div>
+    <p>${snippet}</p>
+  </article>`
+}).join('');
+
+articlesDOM.innerHTML = articlesData;
 
 
 console.log(articles)
