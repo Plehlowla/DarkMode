9fba220060c5181671377158779545a32816f24c
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-05-05 06:27:10 -0500

    import data

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
+
 const btn = document.querySelector('.btn');
+const sectionArticle = document.querySelectorAll('.articles')
 
 btn.addEventListener('click', () => {
     // console.log('test')
     document.documentElement.classList.toggle('dark-theme');
 });
+
+
+console.log(articles)
